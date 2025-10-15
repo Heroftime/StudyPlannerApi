538c3af8e1b6f98945b15976a458b99090671e16
Nomi Smart <sngnomi@outlook.com>
2025-10-15 20:58:23 +0400

    Add project files.

---
+++ Model/Course.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyPlannerApi.Model
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(20)]
+        public string? Code { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [StringLength(50)]
+        public string? Semester { get; set; }
+
+        public int CreditHours { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
