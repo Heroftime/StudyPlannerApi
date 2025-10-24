e3ce538d17da30191c35cf6b3cd8a9457db97f85
Jerrick <jerrickd25@gmail.com>
2025-10-24 20:26:58 +0400

    finished school controller

--- Controllers/SchoolController.cs
+++ Controllers/SchoolController.cs
@@ -21,5 +21,82 @@ namespace StudyPlannerApi.Controllers
         {
             return await _context.Schools.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<School>> GetSchool(int id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            return school;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSchool(int id, School school)
+        {
+            if (id != school.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(school).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchoolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<School>> PostCourse(School school)
+        {
+            _context.Schools.Add(school);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = school.Id }, school);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchool(int id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schools.Remove(school);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
+        private bool SchoolExists(int id)
+            {
+              return _context.Courses.Any(e => e.Id == id);
+            }
+
+
+        
     }
 }
